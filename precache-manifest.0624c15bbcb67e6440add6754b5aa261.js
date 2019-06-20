ff5d5fa742060855626088175c97c5a3dfd38fe1
Giang Nguyen <giang.nguyen.dev@gmail.com>
2019-06-20 22:01:44 +0700

    Updates

---
+++ precache-manifest.0624c15bbcb67e6440add6754b5aa261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2483c6c96ecfcb949590515da5e22a4b",
+    "url": "/react-103-test-ts/index.html"
+  },
+  {
+    "revision": "c2cde9ffc625c7b0e030",
+    "url": "/react-103-test-ts/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "16b654bf6fff58f170e3",
+    "url": "/react-103-test-ts/static/css/main.26b0a4ab.chunk.css"
+  },
+  {
+    "revision": "c2cde9ffc625c7b0e030",
+    "url": "/react-103-test-ts/static/js/2.aa72effb.chunk.js"
+  },
+  {
+    "revision": "16b654bf6fff58f170e3",
+    "url": "/react-103-test-ts/static/js/main.ab2940e3.chunk.js"
+  },
+  {
+    "revision": "981e8e3d976800b1469a",
+    "url": "/react-103-test-ts/static/js/runtime~main.ff6efcc1.js"
+  }
+]);
